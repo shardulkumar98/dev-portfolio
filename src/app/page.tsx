60bde47a38c77188b83d08eda9107884b49c475e
Shardul Kumar <shardul.kumar@thewitslab.com>
2024-01-29 04:12:53 +0530

    fix: change bg color and add title and desc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
+/* eslint-disable react/no-unescaped-entities */
 export default function Home() {
   return (
-    <div className="w-full gap-8 md:gap-5 min-h-[70vh] md:min-h-[83vh] p-5 md:p-24 flex flex-col justify-center">
-      <div className="font-oxygen text-5xl md:text-6xl lg:text-7xl">
-        Hello !
+    <div className="w-full gap-8 md:gap-4 min-h-[70vh] md:min-h-[83vh] p-5 md:p-24 flex flex-col justify-center">
+      <div className="font-oxygen text-2xl">
+        Hello
       </div>
-      <div className="font-oxygen text-6xl  md:text-7xl lg:text-8xl">
-        I am Name
+      <div className="font-oxygen text-3xl  md:text-5xl lg:text-7xl">
+        I'm Shardul Kumar
       </div>
-      <div className="font-oxygen text-2  md:text-3xl lg:text-4xl">
-        Designation
+      <div className="font-oxygen opacity-75 text-2 md:text-xl lg:text-2xl">
+        MERN Stack Developer
+      </div>
+      <div
+        className="opacity-50 w-1/2"
+      >
+        Hi there! My name is Shardul Kumar and I'm a software engineer with over
+        2 years of experience in the industry.
       </div>
     </div>
   );
