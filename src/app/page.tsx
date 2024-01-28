10f7784c26e4efd1572975cdd2ced70017b45aa0
Shardul Kumar <shardul.kumar@thewitslab.com>
2024-01-29 04:42:16 +0530

    fix: add new colors on border and text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,11 +10,11 @@ const Home = () => {
       <div className="font-oxygen text-3xl md:text-5xl lg:text-7xl">
         I'm Shardul Kumar
       </div>
-      <div className="font-oxygen opacity-75 text-[#FC6736] text-2 md:text-xl lg:text-2xl">
+      <div className="font-oxygen text-[#FC6736] text-2 md:text-xl lg:text-2xl">
         MERN Stack Developer
       </div>
       <div
-        className="opacity-50 w-1/2"
+        className="w-1/2"
       >
         Hi there! My name is Shardul Kumar and I'm a software engineer with over
         2 years of experience in the industry.
